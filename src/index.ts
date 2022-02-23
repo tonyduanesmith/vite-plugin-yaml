b860e098acd9e15f2c662a1ca21a25c141d2119c
Tony Ketcham <tony@modyfi.io>
2022-02-23 16:47:47 -0600

    Remove unneeded comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-/* eslint-disable no-console */
 import { load, DEFAULT_SCHEMA } from 'js-yaml';
 import { createFilter } from '@rollup/pluginutils';
 import toSource from 'tosource';
-// import log from 'loglevel';
 
 import type { YAMLException, Schema } from 'js-yaml';
 import type { Plugin } from 'vite';
@@ -12,22 +10,26 @@ export type PluginOptions = {
   /**
    * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
    * should operate on.
+   * 
    * By default all files are targeted.
    */
   include?: FilterPattern;
   /**
    * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
-   * should _ignore_.
+   * should ignore.
+   * 
    * By default no files are ignored.
    */
   exclude?: FilterPattern;
   /**
    * Schema used to parse yaml files.
-   * See https://github.com/nodeca/js-yaml/blob/49baadd52af887d2991e2c39a6639baa56d6c71b/README.md#load-string---options-
+   * 
+   * @see https://github.com/nodeca/js-yaml/blob/49baadd52af887d2991e2c39a6639baa56d6c71b/README.md#load-string---options-
    */
   schema?: Schema;
   /**
    * A function that will be called for error reporting.
+   * 
    * Defaults to `console.warn()`.
    */
   onWarning?: (warning: YAMLException) => void;
@@ -61,7 +63,7 @@ export default (options: PluginOptions = { schema: DEFAULT_SCHEMA }): Plugin => 
       });
 
       return {
-        code: `const data = ${toSource(yamlData)};\nexport default data;`, // toSource is potentially unsafe with user-supplied Modifiers
+        code: `const data = ${toSource(yamlData)};\nexport default data;`,
       };
     }
     return null;
