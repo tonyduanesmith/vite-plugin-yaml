bcad313758b7c6cf730378a5b1d5bb3ba87ac2cc
tonyketcham <tony@karmalies.studio>
2022-08-07 17:04:16 -0500

    Add Vite 3.0 support

--- example/src/importContent.tsx
+++ example/src/importContent.tsx
@@ -1,18 +1,32 @@
+type ESModule = Record<string, unknown> & { default: Record<string, unknown> };
+
 /**
- * Import a directory of YAML files as JSON
+ * Validate that the dynamically imported module has a default export which is an object.
+ */
+export function isESModule(module: unknown): module is ESModule {
+  return typeof module === 'object' && module !== null && 'default' in module;
+}
+
+/**
+ * Dynamically import a directory of YAML files as JSON.
  */
 export function importContent() {
-  const contentModules = import.meta.globEager('./content/**/*.{yml,yaml}');
+  const contentModules = import.meta.glob('./content/**/*.{yml,yaml}', {
+    eager: true,
+  });
 
+  console.log(contentModules);
   if (!contentModules) throw new Error('Could not find any content files');
 
   return Object.entries(contentModules).map(([path, module]) => {
-    // Mild guard against malformed content
-    if (
-      !module ||
-      !module.default?.id ||
-      typeof module.default.id !== 'number'
-    ) {
+    // This is a mild validation on the imported module
+    if (!isESModule(module)) throw new Error(`${path} is not an ES module`);
+
+    // Get the `id` field from the imported YAML files.
+    const { id } = module.default;
+
+    // In this example, we expect the `id` field to be a number.
+    if (!id || typeof id !== 'number') {
       throw new Error(
         `Invalid content: ${path}\n${JSON.stringify(module, null, 2)}`
       );
